71137522ef7ea6717792d5020cdeb2095cd2ab97
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2024-09-23 23:38:43 +0600

    third banner btn's js completed and donation maths works properly

--- js/donate.js
+++ js/donate.js
@@ -74,7 +74,49 @@ document.getElementById('donate-btn2').addEventListener('click', function(){
     const div = document.createElement('div');
         div.classList.add('border-2', 'p-8', 'rounded-xl', 'mb-6');
         div.innerHTML = `
-            <p class="font-bold text-xl text-gray-clr">${donate} Taka is Donated for Flood Relief in Feni,Bangladesh.</p>
+            <p class="font-bold text-xl text-gray-clr">${donate} Taka is Donated for Flood Relief in Feni, Bangladesh.</p>
+            <p class=" text-gray-clr text-opacity-70">Date: ${fullDateAndTime}</p>
+        `
+        document.getElementById('history-container').appendChild(div);
+
+        my_modal_1.showModal();
+})
+
+// input for 3rd donate btn
+document.getElementById('donate-btn3').addEventListener('click', function(){
+    const donate = getInputFieldValueById('input-donate3');
+    const balance = getTextFieldById('account-balance');
+
+    if(isNaN(donate) || donate <= 0){
+        alert('You Did Something Wrong, Please Try again');
+        return;
+    }
+
+    if(balance<=0 || donate>balance){
+        alert('Insufficient Balance');
+        return;
+    }
+
+    const newBalance = balance - donate;
+
+    const singleDonate = document.getElementById('donation3').innerText;
+    const singleDonateNumber = parseFloat(singleDonate);
+
+    const currentSingleDonate = donate + singleDonateNumber;
+    document.getElementById('donation3').innerText = currentSingleDonate;
+    document.getElementById('account-balance').innerText = newBalance;
+
+    document.getElementById('no-history').classList.add('hidden');
+
+    // getting current time and date
+    const currentDate = new Date();
+    const fullDateAndTime = currentDate.toString();
+
+    // Donate History
+    const div = document.createElement('div');
+        div.classList.add('border-2', 'p-8', 'rounded-xl', 'mb-6');
+        div.innerHTML = `
+            <p class="font-bold text-xl text-gray-clr">${donate} Taka is Donated for Injured in the Quota Movement-2024, Bangladesh.</p>
             <p class=" text-gray-clr text-opacity-70">Date: ${fullDateAndTime}</p>
         `
         document.getElementById('history-container').appendChild(div);
